8d0f4b9af47480ac0a6748cc79fd082ef72b6a5f
Guilherme-Henriquee <guihenrics@gmail.com>
2025-03-20 15:08:44 -0300

    adicionando multipenis

--- xesq.py
+++ xesq.py
@@ -2,5 +2,6 @@ nome = input('digite seu nome: ')
 idades = int(input('digite sua idade: '))
 
 soma = 5+5
+mult = 5*5
 divisao = 5/5
-print(f'{nome}, {idades} e a soma de 5+5: {soma}\n A divisao é {divisao}')
+print(f'{nome}, {idades} e a soma de 5+5: {soma}\n A divisao é {divisao} \n A multiplicação é {mult}')
