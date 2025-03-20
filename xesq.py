475a14fe8bc7b43b7582a8d2f7daf80f6dd17e92
Guilherme-Henriquee <guihenrics@gmail.com>
2025-03-20 14:43:48 -0300

    soma consertada

--- xesq.py
+++ xesq.py
@@ -1,6 +1,6 @@
 nome = input('digite seu nome: ')
 idades = int(input('digite sua idade: '))
 
-soma = 5*5
+soma = 5+5
 divisao = 5/5
-print(f'{nome}, {idades} e a soma de 5x5: {soma}\n A divisao é {divisao}')
+print(f'{nome}, {idades} e a soma de 5+5: {soma}\n A divisao é {divisao}')
